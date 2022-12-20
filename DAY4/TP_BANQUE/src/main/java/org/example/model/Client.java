9c97605f6a1014126cde59b2a950ce6833d96aa5
taaaaa111 <taos.tou@gmail.com>
2022-12-20 23:39:27 +0100

    added Client model

--- DAY4/TP_BANQUE/src/main/java/org/example/model/Client.java
+++ DAY4/TP_BANQUE/src/main/java/org/example/model/Client.java
@@ -8,8 +8,56 @@ import java.util.List;
 public class Client {
 
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cname;
+    private String cprenom;
+    private Date cbirthdate;
+    @ManyToMany
+    @JoinTable(name= "client_compte",
+    JoinColumns = @JoinColumn(name="client_id"),
+        inverseJoinColumns = @JoinColumn(name = "compte_id")
+    )
+    private List<Compte> comptes;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getCname() {
+        return cname;
+    }
 
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    public String getCprenom() {
+        return cprenom;
+    }
+
+    public void setCprenom(String cprenom) {
+        this.cprenom = cprenom;
+    }
+
+    public Date getCbirthdate() {
+        return cbirthdate;
+    }
+
+    public void setCbirthdate(Date cbirthdate) {
+        this.cbirthdate = cbirthdate;
+    }
+
+    public List<Compte> getComptes() {
+        return comptes;
+    }
+
+    public void setComptes(List<Compte> comptes) {
+        this.comptes = comptes;
+    }
 }
